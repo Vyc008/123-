e20653b999bd0995c80600be8723100490d4f451
Vyc008 <97494208+Vyc008@users.noreply.github.com>
2026-06-27 21:06:59 +0700

    Update Bonhac&ctgv-thw.lua

--- Bonhac&ctgv-thw.lua
+++ Bonhac&ctgv-thw.lua
@@ -213,7 +213,7 @@ StepListScroll.Size = UDim2.new(1, 0, 0, 35); StepListScroll.Position = UDim2.ne
 local StepLayout = Instance.new("UIListLayout", StepListScroll)
 StepLayout.FillDirection = Enum.FillDirection.Horizontal; StepLayout.SortOrder = Enum.SortOrder.LayoutOrder; StepLayout.Padding = UDim.new(0, 5); StepLayout.VerticalAlignment = Enum.VerticalAlignment.Center
 
-local presetSpeeds = {"0.1", "0.5", "0.10", "0.15", "0.20", "0.25", "0.50"}
+local presetSpeeds = {"0.01", "0.05", "0.10", "0.15", "0.20", "0.25", "0.50"}
 local stepButtons = {}
 for _, v in ipairs(presetSpeeds) do
     local btn = Instance.new("TextButton", StepListScroll)
